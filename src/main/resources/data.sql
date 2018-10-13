f5121efa122810d4ee04a0d31975183a0db8aff8
Martiniele <ielegeek@sina.cn>
2018-10-13 18:29:21 +0800

    更新data.sql相关字段的长度。

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,7 +4,7 @@ CREATE TABLE `sys_log` (
   `create_by` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '操作时间',
   `remark` varchar(255) NOT NULL DEFAULT '' COMMENT '操作内容',
   `operate_url` varchar(90) NOT NULL DEFAULT '' COMMENT '操作的访问地址',
-  `operate_by` varchar(20) DEFAULT '' COMMENT '操作的浏览器',
+  `operate_by` varchar(80) DEFAULT '' COMMENT '操作的浏览器',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
